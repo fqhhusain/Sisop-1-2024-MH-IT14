d98b95f36fb24f7898e73f9cbf1640eb20377b5c
SyahmiAsh <149950475+SyahmiAsh@users.noreply.github.com>
2024-03-25 15:52:37 +0700

    Create login.sh

---
+++ soal_2/login.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+# Mengecek email yang telah teregistrasi
+cek_email() {
+    if ! grep -q "^$email:" /home/ash23/Downloads/soalsisop/users/users.txt; then
+        echo "Email not registered!"
+        exit 1
+    fi
+}
+
+# Mengecek question yang telah teregistrasi
+cek_question() {
+    if ! grep -q "^$email:" /home/ash23/Downloads/soalsisop/users/users.txt | cut -d':' -f5; then
+        echo "Question not found!"
+        exit 1
+    fi
+}
+
+# Mengecek answer yang telah teregistrasi
+cek_answer() {
+    if ! grep -q "^$answer:" /home/ash23/Downloads/soalsisop/users/users.txt | cut -d':' -f5; then
+        echo "Answer not found!"
+        exit 1
+    fi
+}
+
+# Mengubah pass dari encrypt base64 
+de_pass() {
+    echo "$sav_pass" | base64 -d
+}
+
+# Mengecek pass apakah sesuai dgn yg sudah teregistrasi
+validasi_login() {
+    sav_pass=$(grep "^$email:" /home/ash23/Downloads/soalsisop/users/users.txt | cut -d':' -f5)
+    ded_pass=$(de_pass "$sav_pass")
+
+    if [[ "$password" != "$ded_pass" ]]; then
+        echo "Wrong password!"
+        echo "$(date +"[%d/%m/%y %H:%M:%S]") [LOGIN FAILED] with email [$email] logged in failed." >> /home/ash23/Downloads/soalsisop/users/auth.log
+        exit 1
+    fi
+}
+
+# Mengecek pass apakah sesuai email, question, answer dgn yg sudah teregistrasi
+validasi_forget() {
+    sav_pass=$(grep "^$email:" /home/ash23/Downloads/soalsisop/users/users.txt | cut -d':' -f5)
+    ded_pass=$(de_pass "$sav_pass")
+    echo "Your password is:" "$ded_pass"
+}
+
+cek_admin() {
+    if [[ $email == *admin* ]]; then
+        echo "Admin Menu"
+        echo "1. Add User"
+        echo "2. Edit User"
+        echo "3. Delete User"
+        echo "4. Logout"
+        echo -n "jawab: "
+        read jawaban
+
+        case "$jawaban" in
+          "1")
+            nano /home/ash23/Downloads/soalsisop/users/users.txt     
+            ;;
+          "2")
+            nano /home/ash23/Downloads/soalsisop/users/users.txt 
+            ;;
+          "3")
+            nano /home/ash23/Downloads/soalsisop/users/users.txt 
+            ;;
+          "4")
+            echo "logout"
+            ;;
+        esac
+    else
+        exit 1
+    fi
+}
+
+echo "Welcome to Login System"
+echo "1. Login"
+echo "2. Forget Password"
+echo -n "jawab: "
+read jawaban
+
+case "$jawaban" in
+  "1")
+    read -p "Enter your email: " email
+    cek_email "$email"
+    read -s -p "Password: " password
+    echo
+
+    validasi_login "$email" "$password"
+
+    echo "Login successful"
+    cek_admin "$email"
+
+    echo "$(date +"[%d/%m/%y %H:%M:%S]") [LOGIN SUCCESS] with email [$email] logged in successfully." >> /home/ash23/Downloads/soalsisop/users/auth.log
+     
+    ;;
+  "2")
+    echo "Forget password?"
+    echo "Enter your email:"
+    read email
+    cek_email "$email"
+    read -p "Security Question: " question
+    cek_question
+    read -p "Enter your answer: " answer
+    cek_answer
+
+    validasi_forget "$question"
+
+    ;; 
+    *)
+    echo "!! ERROR !!"
+    ;;
+esac
+
